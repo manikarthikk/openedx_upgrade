4b3a9b1b9c82561da856a92e81c79e794c8b09ef
Manikarthik Konakalla <v-mankon@microsoft.com>
2017-01-25 13:54:08 -0800

    Delete sqlmigrate_cms.sh

--- sqlmigrate_cms.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read line; do
-cd /edx/app/edxapp/edx-platform
-source /edx/app/edxapp/edxapp_env
-sudo -E -u edxapp env "PATH=$PATH" /edx/app/edxapp/venvs/edxapp/bin/python manage.py cms sqlmigrate --settings=aws $line
-done < cms_upgrade.log > cms_migration_sql.sql
-
