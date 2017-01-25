571a2fffbe6d213de029e76c5b5546f8bc27c3f2
Manikarthik Konakalla <v-mankon@microsoft.com>
2017-01-25 13:54:21 -0800

    Delete sqlmigrate_lms.sh

--- sqlmigrate_lms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while read line; do
-
-cd /edx/app/edxapp/edx-platform
-source /edx/app/edxapp/edxapp_env
-sudo -E -u edxapp env "PATH=$PATH" /edx/app/edxapp/venvs/edxapp/bin/python manage.py lms sqlmigrate --settings=aws $line
-done < lms_upgrade.log > lms_migration_sql.sql
-
