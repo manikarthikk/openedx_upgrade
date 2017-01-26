a302bab0d089e36c397fa05d258eab2f4f701215
Manikarthik Konakalla <v-mankon@microsoft.com>
2017-01-26 15:56:40 -0800

    Update sqlmigrate_lms.sh

--- sqlmigrate_lms.sh
+++ sqlmigrate_lms.sh
@@ -4,5 +4,6 @@ while read line; do
 
 cd /edx/app/edxapp/edx-platform
 source /edx/app/edxapp/edxapp_env
+echo ============$line=============================
 sudo -E -u edxapp env "PATH=$PATH" /edx/app/edxapp/venvs/edxapp/bin/python manage.py lms sqlmigrate --settings=aws $line
 done < lms_upgrade.log > lms_migration_sql.sql
