2cb6d64a8f0e47c7d0ee5f8ec9882f5f3788affd
Manikarthik Konakalla <v-mankon@microsoft.com>
2017-01-27 14:20:43 -0800

    Update sqlmigrate_cms.sh

--- sqlmigrate_cms.sh
+++ sqlmigrate_cms.sh
@@ -3,5 +3,5 @@ while read line; do
 cd /edx/app/edxapp/edx-platform
 source /edx/app/edxapp/edxapp_env
 echo "/*============$line=============================*/"
-sudo -E -u edxapp env "PATH=$PATH" /edx/app/edxapp/venvs/edxapp/bin/python manage.py cms sqlmigrate --settings=aws $line
-done < cms_upgrade.log > cms_migration_sql.sql
+sudo -E -u edxapp env "PATH=$PATH" /edx/app/edxapp/venvs/edxapp/bin/python manage.py cms sqlmigrate --settings=aws tagging 0001
+done >> cms_migration_sql.sql
