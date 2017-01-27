ea561a47270b6e249e1d05c3520fa8ab7e862862
Manikarthik Konakalla <v-mankon@microsoft.com>
2017-01-27 15:51:43 -0800

    added comment

--- run_sqlmigrate.sh
+++ run_sqlmigrate.sh
@@ -7,7 +7,7 @@ source /edx/app/edxapp/edxapp_env
 echo "/*============$line=============================*/"
 sudo -E -u edxapp env "PATH=$PATH" /edx/app/edxapp/venvs/edxapp/bin/python manage.py lms sqlmigrate --settings=aws $line
 done < lms_upgrade.log > /tmp/lms_migration_sql.sql
-
+# the below code is hard coded since tagging migration is missing in the upgrade.log file
 cd /edx/app/edxapp/edx-platform
 source /edx/app/edxapp/edxapp_env
 echo "/*============$line=============================*/" >>/tmp/lms_migration_sql.sql
