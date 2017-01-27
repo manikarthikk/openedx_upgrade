0a5c08cdc64365bde081d2afbccc45e4562131fd
Manikarthik Konakalla <v-mankon@microsoft.com>
2017-01-27 15:36:33 -0800

    added this in sqlmigrate_lms.sh

--- sqlmigrate_cms.sh
+++
@@ -1,8 +0,0 @@
-while read line; do
-
-sudo su edxapp -s /bin/bash
-cd ~ && source edxapp_env    
-cd edx-platform 
-echo "/*============$line=============================*/"
-python manage.py cms sqlmigrate --settings=aws tagging 0001
-done > cms_migration_sql.sql
