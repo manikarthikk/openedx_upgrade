093eb00e16044e656d2a8e0ebcb30fd48895d073
Manikarthik Konakalla <v-mankon@microsoft.com>
2017-10-30 19:10:07 -0700

    Update land2.py

--- land2.py
+++ land2.py
@@ -15,9 +15,9 @@ def sync_course_catalog():
     key_vault_url = "https://manikeyvault3.vault.azure.net"
     key_vault_resource = "https://vault.azure.net"
     key_vault_headers = dict(MetaData='true')
-    edx_api_key = k.get_key_vault_secret(k.get_access_token(key_vault_resource,key_vault_url,key_vault_headers), key_vault_url,'edx-api-key')
-    edx_access_token = k.get_key_vault_secret(k.get_access_token(key_vault_resource,key_vault_url,key_vault_headers), key_vault_url,'edx-access-token')
-    edx_headers = dict(Authorization='Bearer' + edx_access_token, X_API_KEY=edx_api_key)
+    edx_api_key = k.get_key_vault_secret(k.get_access_token(), key_vault_url,'edx-api-key')
+    edx_access_token = k.get_key_vault_secret(k.get_access_token(), key_vault_url,'edx-access-token')
+    edx_headers = dict(Authorization='Bearer' + ' ' + edx_access_token, X_API_KEY=edx_api_key)
     print(k.get_course_catalog_data(edx_course_catalog_url,edx_headers))
 
 if __name__ == "__main__":
