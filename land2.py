8a40455533698bf2127f37a0bc769de26257b0db
Manikarthik Konakalla <v-mankon@microsoft.com>
2017-10-30 22:10:15 -0700

    Update land2.py

--- land2.py
+++ land2.py
@@ -22,27 +22,29 @@ def sync_course_catalog():
     5) POST the mapped data to L&D Catalog_UpdateMultipleCatalogAsync API
 
     """
-    k = land.LdIntegration(logger=LOG)
+    # initialize the key variables
+    catalog_service = land.LdIntegration(logger=LOG)
     edx_course_catalog_url = "https://lms-lexoxabvtc99-tm.trafficmanager.net/api/courses/v1/courses/"
     key_vault_url = "https://manikeyvault3.vault.azure.net"
     landd_catalog_url = 'https://ldserviceuat.microsoft.com/Consumption/exptrack'
 
     # get secrets from Azure Key Vault
-    edx_api_key = k.get_key_vault_secret(k.get_access_token(), key_vault_url, 'edx-api-key')
-    edx_access_token = k.get_key_vault_secret(k.get_access_token(), key_vault_url, 'edx-access-token')
-    landd_authorityhosturl = k.get_key_vault_secret(k.get_access_token(), key_vault_url, 'landd-authorityhosturl')
-    landd_clientid = k.get_key_vault_secret(k.get_access_token(), key_vault_url, 'landd-clientid')
-    landd_clientsecret = k.get_key_vault_secret(k.get_access_token(), key_vault_url, 'landd-clientsecret')
-    landd_resource = k.get_key_vault_secret(k.get_access_token(), key_vault_url, 'landd-resource')
-    landd_tenant = k.get_key_vault_secret(k.get_access_token(), key_vault_url, 'landd-tenant')
-    landd_subscription_key = k.get_key_vault_secret(k.get_access_token(), key_vault_url, 'landd-subscription-key')
+    edx_api_key = catalog_service.get_key_vault_secret(catalog_service.get_access_token(), key_vault_url, 'edx-api-key')
+    edx_access_token = catalog_service.get_key_vault_secret(catalog_service.get_access_token(), key_vault_url, 'edx-access-token')
+    landd_authorityhosturl = catalog_service.get_key_vault_secret(catalog_service.get_access_token(), key_vault_url, 'landd-authorityhosturl')
+    landd_clientid = catalog_service.get_key_vault_secret(catalog_service.get_access_token(), key_vault_url, 'landd-clientid')
+    landd_clientsecret = catalog_service.get_key_vault_secret(catalog_service.get_access_token(), key_vault_url, 'landd-clientsecret')
+    landd_resource = catalog_service.get_key_vault_secret(catalog_service.get_access_token(), key_vault_url, 'landd-resource')
+    landd_tenant = catalog_service.get_key_vault_secret(catalog_service.get_access_token(), key_vault_url, 'landd-tenant')
+    landd_subscription_key = catalog_service.get_key_vault_secret(catalog_service.get_access_token(), key_vault_url, 'landd-subscription-key')
 
+    # construct headers using key vault secrets
     edx_headers = dict(Authorization='Bearer' + ' ' + edx_access_token, X_API_KEY=edx_api_key)
-    catalog_data = k.get_course_catalog_data(edx_course_catalog_url, edx_headers)
+
     headers = {
         'Content-Type': 'application/json',
         'Ocp-Apim-Subscription-Key': landd_subscription_key,
-        'Authorization': k.get_access_token_ld(
+        'Authorization': catalog_service.get_access_token_ld(
             landd_authorityhosturl,
             landd_tenant,
             landd_resource,
@@ -51,7 +53,8 @@ def sync_course_catalog():
             )
         }
 
-    k.post_data_ld(landd_catalog_url, headers, k.catalog_data_mapping(16, catalog_data))
+    catalog_data = catalog_service.get_course_catalog_data(edx_course_catalog_url, edx_headers)
+    catalog_service.post_data_ld(landd_catalog_url, headers, catalog_service.catalog_data_mapping(16, catalog_data))
 
 if __name__ == "__main__":
     sync_course_catalog()
